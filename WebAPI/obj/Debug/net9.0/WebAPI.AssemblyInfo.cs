68b440cd1dae525400dbe758b4ab07ba47be97fd
Busthenut <kevin245312@gmail.com>
2025-01-30 11:41:03 +0100

    k

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32f68af57983e69631b02e35194dea5a03cfb00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
